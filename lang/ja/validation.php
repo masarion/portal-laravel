d196d883fecee4946847251dd0f1ee2a51d69381
masarion <150416564+masarion@users.noreply.github.com>
2026-06-14 06:50:30 +0900

    ログイン画面を日本語化・エラーメッセージ日本語化

---
+++ lang/ja/validation.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    'accepted'             => ':attribute を承認してください。',
+    'email'                => ':attribute には有効なメールアドレスを指定してください。',
+    'max'                  => [
+        'string' => ':attribute は :max 文字以下で入力してください。',
+    ],
+    'min'                  => [
+        'string' => ':attribute は :min 文字以上で入力してください。',
+    ],
+    'required'             => ':attribute は必須です。',
+    'unique'               => ':attribute はすでに使用されています。',
+    'confirmed'            => ':attribute と確認用の値が一致しません。',
+    'string'               => ':attribute は文字列で入力してください。',
+
+    'attributes' => [
+        'email'    => 'メールアドレス',
+        'password' => 'パスワード',
+        'name'     => '名前',
+    ],
+];
